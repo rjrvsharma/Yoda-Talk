6a39732b9f9f3ada23a6df196dad2b86958dd2d1
Ravishankar Vishwakarma <rjrvsharma@gmail.com>
2021-06-06 14:16:07 +0530

    yoda app done

---
+++ app.js
@@ -0,0 +1,34 @@
+  
+var btntranslate = document.querySelector("#btn-translate");
+
+var txtinput = document.querySelector("#txt-input");
+
+var outputdiv = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input
+}
+
+
+function errorHandler(error){
+  console.log("error occured",error);
+  alert("something wrong with server! try agin bro")
+}
+
+
+function clickHandler() {
+ var inputText = txtinput.value;
+
+ fetch(getTranslationURL(inputText))
+ .then(response => response.json())
+ .then(json => {
+   var translatedText= json.contents.translated;
+   outputdiv.innerText = translatedText;
+ })
+ .catch(errorHandler)
+};
+
+btntranslate.addEventListener("click" , clickHandler)
